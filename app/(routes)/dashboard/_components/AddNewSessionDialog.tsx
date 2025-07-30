bca9d06014a61d50dfda028598e3a03abbb2555b
Omkaar <omdb2004@gmail.com>
2025-07-30 17:26:27 +0530

    T-8

--- app/(routes)/dashboard/_components/AddNewSessionDialog.tsx
+++ app/(routes)/dashboard/_components/AddNewSessionDialog.tsx
@@ -1,56 +1,5 @@
-// import React from "react";
-// import {
-//   Dialog,
-//   DialogContent,
-//   DialogDescription,
-//   DialogFooter,
-//   DialogHeader,
-//   DialogTitle,
-//   DialogTrigger,
-// } from "@/components/ui/dialog";
-// import { Button } from "@/components/ui/button";
-// import { IconPlus } from "@tabler/icons-react";
-// import { Textarea } from "@/components/ui/textarea";
-// import { DialogClose } from "@radix-ui/react-dialog";
-// function AddNewSessionDialog() {
-//   return (
-//     <Dialog>
-//       <DialogTrigger>
-//         <Button className="mt-4 flex items-center justify-center gap-2 rounded-xl group bg-primary text-white hover:bg-primary/90 transition-all">
-//           <IconPlus
-//             size={18}
-//             className="transition-transform duration-200 group-hover:rotate-90"
-//           />
-//           <span className="font-medium">Start a Consultation</span>
-//         </Button>
-//       </DialogTrigger>
-//       <DialogContent>
-//         <DialogHeader>
-//           <DialogTitle>Add Basic Details</DialogTitle>
-//           <DialogDescription asChild>
-//             <div>
-//               <h2>Add Symtoms or ANy Other Deatils</h2>
-//               <Textarea
-//                 placeholder="Add detail here"
-//                 className="h-[200px] mt-1"
-//               />
-//             </div>
-//           </DialogDescription>
-//         </DialogHeader>
-//         <DialogFooter>
-//           <DialogClose>
-//             <Button variant={"outline"}>Cancel</Button>
-//           </DialogClose>
-
-//           <Button>Start</Button>
-//         </DialogFooter>
-//       </DialogContent>
-//     </Dialog>
-//   );
-// }
-
-// export default AddNewSessionDialog;
-import React from "react";
+"use clinet";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -61,11 +10,13 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { IconPlus } from "@tabler/icons-react";
+import { IconArrowRight, IconPlus } from "@tabler/icons-react";
 import { Textarea } from "@/components/ui/textarea";
 import { DialogClose } from "@radix-ui/react-dialog";
 
 function AddNewSessionDialog() {
+  const [note, setNote] = useState<string>();
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -87,7 +38,11 @@ function AddNewSessionDialog() {
         </DialogHeader>
 
         <div className="mt-4">
-          <Textarea placeholder="Add detail here" className="h-[200px]" />
+          <Textarea
+            placeholder="Add detail here"
+            className="h-[200px]"
+            onChange={(e) => setNote(e.target.value)}
+          />
         </div>
 
         <DialogFooter>
@@ -96,7 +51,10 @@ function AddNewSessionDialog() {
               Cancel
             </Button>
           </DialogClose>
-          <Button type="button">Start</Button>
+          <Button className="mt-4 flex justify-between items-center group rounded-xl">
+            <span>Next</span>
+            <IconArrowRight className="group-hover:translate-x-1 transition-transform duration-200" />
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
