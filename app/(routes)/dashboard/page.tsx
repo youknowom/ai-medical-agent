d013d29341f8bf08cc6d395a8ac7a1dc3ff19d5c
Omkaar <omdb2004@gmail.com>
2025-07-30 16:54:03 +0530

    T-5

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,22 +1,3 @@
-// import React from "react";
-// import HistoryList from "./_components/HistoryList";
-// import { Button } from "@/components/ui/button";
-// import DoctorsAgentList from "./_components/DoctorsAgentList";
-
-// const Dashboard = () => {
-//   return (
-//     <div>
-//       <div className="flex justify-between items-center">
-//         <h2 className="font-bold text-2xl">My Dashboard</h2>
-//         <Button className="rounded-xl">+ Start a Consultation</Button>
-//       </div>
-//       <HistoryList />
-//       <DoctorsAgentList />
-//     </div>
-//   );
-// };
-
-// export default Dashboard;
 import React from "react";
 import HistoryList from "./_components/HistoryList";
 import { Button } from "@/components/ui/button";
