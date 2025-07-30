f5e1c7f89e52253ea6a7f1059de00f9825cfed2c
Omkaar <omdb2004@gmail.com>
2025-07-30 19:23:57 +0530

    T-12

--- app/api/suggest-doctors/route.tsx
+++ app/api/suggest-doctors/route.tsx
@@ -1,3 +1,22 @@
 import { NextRequest } from "next/server";
+import openai from "@/config/OpenAiModel";
+import { AIDoctorAgents } from "@/shared/list";
 
-export async function POST(req: NextRequest) {}
+export async function POST(req: NextRequest) {
+  const { notes } = await req.json();
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "google/gemini-2.5-flash-lite",
+      messages: [
+        { role: "system", content: JSON.stringify(AIDoctorAgents) },
+        {
+          role: "user",
+          content:
+            "User Notes/Symptoms:" +
+            notes +
+            ", Depends on user notes and symptoms, Please suggest list of doctors, Return Object in JSON only",
+        },
+      ],
+    });
+  } catch (e) {}
+}
--- config/OpenAiModel.tsx
+++ config/OpenAiModel.tsx
@@ -1,5 +1,8 @@
 import OpenAI from "openai";
+
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.OPEN_ROUTER_API_KEY,
 });
+
+export default openai;
