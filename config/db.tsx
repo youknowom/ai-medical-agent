631eb52e0b6f73bf0d1b9314b454bcd785f4d592
Omkaar <omdb2004@gmail.com>
2026-05-08 23:41:13 +0530

    feat: initialize Neon database connection using Drizzle ORM

--- config/db.tsx
+++ config/db.tsx
@@ -1,5 +1,5 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-
+//hi
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql });
